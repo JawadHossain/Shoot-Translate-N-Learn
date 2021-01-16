b6c21461cdb17d3e406e0482496b83af0e48aa20
Jawad Hossain <jhossain@ualberta.ca>
2021-01-16 16:37:11 -0700

    Create Android Project

---
+++ app/src/main/java/shootTranslateLearn/customview/ResultsView.java
@@ -0,0 +1,9 @@
+package shootTranslateLearn.customview;
+
+import java.util.List;
+
+import shootTranslateLearn.tensorflow.lite.examples.detection.tflite.Detector;
+
+public interface ResultsView {
+  public void setResults(final List<Detector.Recognition> results);
+}
