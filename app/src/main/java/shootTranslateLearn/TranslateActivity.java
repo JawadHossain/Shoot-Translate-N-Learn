d1b24961d4da6b01a5b303a39433e6bca1901a85
iamOlivier <olivieryannick5@gmail.com>
2021-01-16 23:59:14 -0700

    Update translation

--- app/src/main/java/shootTranslateLearn/TranslateActivity.java
+++ app/src/main/java/shootTranslateLearn/TranslateActivity.java
@@ -9,11 +9,16 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -36,6 +41,10 @@ public class TranslateActivity extends AppCompatActivity {
     private ImageButton speakButton;
     private TextToSpeech textToSpeech;
     private ProgressBar progressBar;
+    private Spinner spinner;
+    private String targetLang;
+    private Locale localeLang;
+    private String detectedObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,20 +53,65 @@ public class TranslateActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        String detectedObject = bundle.getString("image");
+        detectedObject = bundle.getString("image");
 
         selected = findViewById(R.id.objectDetected_textview);
         image = findViewById(R.id.imageView2);
         translate_bttn = findViewById(R.id.translate_button);
         translated = findViewById(R.id.output_textview);
         speakButton = findViewById(R.id.imageButton);
+        spinner = findViewById(R.id.language_spinner);
+        progressBar = findViewById(R.id.progressBar);
+
+        selected.setText(detectedObject);
+        translate_bttn.setEnabled(false);
+
+        String[] languages = {"Choose language", "French", "German", "Spanish"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, languages);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String language = adapter.getItem(position);
+                if (language.equals("French")) {
+                    targetLang = TranslateLanguage.FRENCH;
+                    localeLang = Locale.FRENCH;
+                } else if (language.equals("German")) {
+                    targetLang = TranslateLanguage.GERMAN;
+                    localeLang = Locale.GERMAN;
+                } else {
+                    targetLang = TranslateLanguage.SPANISH;
+                    localeLang = Locale.GERMAN;
+                }
+
+                if (!language.equals("Choose language")) {
+                    translate_bttn.setEnabled(true);
+                }
+
+                TranslatorOptions options =
+                        new TranslatorOptions.Builder()
+                                .setSourceLanguage(TranslateLanguage.ENGLISH)
+                                .setTargetLanguage(targetLang)
+                                .build();
+
+                final Translator translator = Translation.getClient(options);
+
+                progressBar.setVisibility(View.VISIBLE);
+                translateText(translator);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         textToSpeech = new TextToSpeech(getApplicationContext(),
                 new TextToSpeech.OnInitListener() {
                     @Override
                     public void onInit(int status) {
                         if (status == TextToSpeech.SUCCESS) {
-                            int lang = textToSpeech.setLanguage(Locale.ENGLISH);
+                            int lang = textToSpeech.setLanguage(localeLang);
                             textToSpeech.setSpeechRate((float) 0.5);
                         }
                     }
@@ -70,24 +124,21 @@ public class TranslateActivity extends AppCompatActivity {
             }
         });
 
-        progressBar = findViewById(R.id.progressBar);
-        selected.setText(detectedObject);
+        translate_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        TranslatorOptions options =
-                new TranslatorOptions.Builder()
-                        .setSourceLanguage(TranslateLanguage.ENGLISH)
-                        .setTargetLanguage(TranslateLanguage.GERMAN)
-                        .build();
+            }
+        });
 
-        progressBar.setVisibility(View.VISIBLE);
+    }
 
-        final Translator englishGermanTranslator = Translation.getClient(options);
-
+    private void translateText(Translator translator) {
         DownloadConditions conditions = new DownloadConditions.Builder()
                 .requireWifi()
                 .build();
 
-        englishGermanTranslator.downloadModelIfNeeded(conditions)
+        translator.downloadModelIfNeeded(conditions)
                 .addOnSuccessListener(
                         new OnSuccessListener<Void>() {
                             @Override
@@ -95,17 +146,18 @@ public class TranslateActivity extends AppCompatActivity {
                             {
                                 // Model downloaded successfully. Okay to start translating.
                                 // (Set a flag, unhide the translation UI, etc.)
-                                progressBar.setVisibility(View.GONE);
-                                Log.d("Jawad", "downloaded ");
+                                Toast.makeText(getApplicationContext(), "Downloaded necessary packages", Toast.LENGTH_SHORT).show();
 
-                                englishGermanTranslator.translate(detectedObject)
+                                translator.translate(detectedObject)
                                         .addOnSuccessListener(
                                                 new OnSuccessListener<String>() {
 
                                                     @Override
-                                                    public void onSuccess(String text)
+                                                    public void onSuccess(String translatedText)
                                                     {
-                                                        Log.d("Jawad", text.toString()+"hello");
+                                                        progressBar.setVisibility(View.GONE);
+                                                        translated.setText(translatedText);
+                                                        Log.d("Jawad", translatedText.toString()+"hello");
                                                     }
 
                                                 })
@@ -113,6 +165,8 @@ public class TranslateActivity extends AppCompatActivity {
                                                 new OnFailureListener() {
                                                     @Override
                                                     public void onFailure(@NonNull Exception e) {
+                                                        translated.setError("Something went wrong :(");
+                                                        progressBar.setVisibility(View.GONE);
                                                         Log.d("Jawad", "failed translating");
 
                                                     }
@@ -124,8 +178,8 @@ public class TranslateActivity extends AppCompatActivity {
                         new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
-                                Log.d(TAG, "Model couldn’t be downloaded or other internal error.");
                                 progressBar.setVisibility(View.GONE);
+                                Log.d(TAG, "Model couldn’t be downloaded or other internal error.");
                             }
                         });
     }
